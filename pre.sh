3b355489150ca87f01eb4acfc35fd9aa7192c14e
Mann <chanmannlim@gmail.com>
2015-10-31 17:16:55 -0500

    save wavlist file

--- pre.sh
+++ pre.sh
@@ -7,9 +7,9 @@ WAV='wav'
 mkdir -p mfcc tmp logs
 
 # generate scripts/wav2mfcc
-ls -1d $WAV/* > tmp/wavlist
-sed -e "/^$WAV\//mfcc\//" -e 's/.wav$/.mfc/' tmp/wavlist > tmp/mfclist
-paste tmp/wavlist tmp/mfclist > scripts/wav2mfcc
+ls -1d $WAV/* > scripts/wavlist
+sed -e "s/^$WAV\//mfcc\//" -e 's/.wav$/.mfc/' scripts/wavlist > tmp/mfclist
+paste scripts/wavlist tmp/mfclist > scripts/wav2mfcc
 
 # feature extraction
 HCopy -T 3 -C configs/hcopy.conf -S scripts/wav2mfcc > logs/hcopy_trn.log
