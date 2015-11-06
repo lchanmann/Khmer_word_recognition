da5e9220b5ae19dc5e38e3094b7ed2e9e4177ec6
Mann <chanmannlim@gmail.com>
2015-11-06 13:11:07 -0600

    viterbi alignment

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 # init.sh - HMM flat start initialization 
 
 # create required directories
-mkdir -p models models/hmm_0 models/hmm_2
+mkdir -p models/hmm_0 models/hmm_2
 
 # flat-start initialization
 HCompV \
@@ -10,6 +10,7 @@ HCompV \
  -S scripts/mfclist models/proto \
  > logs/hcompv_hmm_0.log
 
+# initialize each hmm with the global estimated mean and variance
 head -n 3 models/hmm_0/proto > models/hmm_0/models.mmf
 for phone in `cat phones/all.phe`
 do
@@ -33,28 +34,31 @@ HERest \
  -S scripts/mfclist -I labels/phoneme.mlf phones/all.phe \
  > logs/herest_hmm_0.log
 
+# update HMM parameters
 HHEd \
  -T 1 -H models/hmm_0/models.mmf -M models/hmm_0 \
  commands/sil.hed phones/all.phe \
  > logs/hhed_hmm_0.log
 
-# HERest \
-#  -T 1 -H models/hmm_0/models.mmf -M models/hmm_0 \
-#  -C configs/herest.conf -w 1 -t 120.0 60.0 960.0 \
-#  -S scp/khmer.scp -I mlf/khmer.mlf phe/khmer.phe \
-#  > models/hmm_0/herest.log
-# HERest \
-#  -T 1 -H models/hmm_0/models.mmf -M models/hmm_0 \
-#  -C configs/herest.conf -w 1 -t 120.0 60.0 960.0 \
-#  -S scp/khmer.scp -I mlf/khmer.mlf phe/khmer.phe \
-#  > models/hmm_0/herest.log
+# parameter re-estimation
+HERest \
+ -T 1 -H models/hmm_0/models.mmf -M models/hmm_0 \
+ -C configs/herest.conf -w 1 -t 120.0 60.0 960.0 \
+ -S scripts/mfclist -I labels/phoneme.mlf phones/all.phe \
+ > logs/herest_hmm_0.log
+HERest \
+ -T 1 -H models/hmm_0/models.mmf -M models/hmm_0 \
+ -C configs/herest.conf -w 1 -t 120.0 60.0 960.0 \
+ -S scripts/mfclist -I labels/phoneme.mlf phones/all.phe \
+ > logs/herest_hmm_0.log
 
-
-# HVite \
-#  -T 1 -a -l '*' -I mlf/khmerwrd.mlf -i mlf/khmeralgn.mlf \
-#  -C configs/hvite.cfg -m -b SIL -o SW -y lab \
-#  -S scp/khmer.scp -H models/hmm_0/models.mmf dct/khmer.dct.withsil phe/khmer.phe \
-#  > models/hmm_0/hvite.log
+# viterbi alignment
+HVite \
+ -T 1 -a -l '*' -I labels/words.mlf -i labels/phoneme_with_alignment.mlf \
+ -C configs/hvite.conf -m -b SIL -o SW -y lab \
+ -S scripts/mfclist -H models/hmm_0/models.mmf \
+ dictionary/dictionary.dct.withsil phones/all.phe \
+ > logs/hvite_hmm_0.log
 
 # HERest \
 #  -T 1 -H models/hmm_0/models.mmf -M models/hmm_0 \
