af48748fdcfa39a3b0444e00a761dadba49b7e5c
Mann <chanmannlim@gmail.com>
2015-11-06 00:02:13 -0600

    generate neccessary files

--- pre.sh
+++ pre.sh
@@ -2,7 +2,7 @@
 WAV='wav'
 
 # create required directories
-mkdir -p mfcc tmp logs
+mkdir -p mfcc lm tmp logs
 
 # generate scripts/wav2mfcc
 ls -1d $WAV/* > scripts/wavlist
@@ -10,24 +10,24 @@ sed -e "s/^$WAV\//mfcc\//" -e 's/.wav$/.mfc/' scripts/wavlist > tmp/mfclist
 paste scripts/wavlist tmp/mfclist > scripts/wav2mfcc
 
 # feature extraction
-HCopy -T 3 -C configs/hcopy.conf -S scripts/wav2mfcc > logs/hcopy_pre.log
+HCopy -T 3 -C configs/hcopy.conf -S scripts/wav2mfcc > logs/hcopy_train.log
 
 # word -> phoneme level label generation
-HLEd -T 1 -l '*/' -d dictionary.dct -i phoneme.mlf hled.cmd words.mlf > logs/hled.log
+HLEd -T 1 -l '*/' -d dictionary/dictionary.dct -i labels/phoneme.mlf edfile.led labels/words.mlf > logs/hled.log
 
 # phoneme list generation
-cat phoneme.mlf | grep '^[a-z]' | sort -u > all.phe
-cat phoneme.mlf | grep -v '^(\#|\"|\.|sil)'| sort -u > all.phe.nosil
-cat phoneme.mlf | grep -v '^(\#|\"|\.)'| sort -u > dictionary.wrd
-echo -e '!ENTER\n!EXIT' >> dictionary.wrd
+cat labels/phoneme.mlf | grep '^[a-z]' | sort -u > phones/all.phe
+cat phones/all.phe | grep -v '^sil' | sort -u > phones/all.phe.nosil
+cat labels/words.mlf | grep -v '^[#".]'| sort -u > dictionary/dictionary.wrd
+echo -e '!ENTER\n!EXIT' >> dictionary/dictionary.wrd
 
-# # with-silence dictionary generation
-# cp dct/khmer.dct dct/khmer.dct.withsil
-# echo -e 'SIL\t[]\tsil' >> dct/khmer.dct.withsil
+# dictionary.dct.withsil generation
+cp dictionary/dictionary.dct dictionary/dictionary.dct.withsil
+echo -e 'SIL\t[]\tsil' >> dictionary/dictionary.dct.withsil
 
-# # language model generation # should use only training data instead!!
-# HLStats -T 1 -b lm/khmer_bi.lm -o -t 1 dct/khmer.wrd mlf/khmerwrd.mlf > lm/hlstats.log
-# HBuild -T 1 -n lm/khmer_bi.lm dct/khmer.wrd lm/khmer_bi.lat > lm/hbuild.log
+# bigram language model generation # should use only training data instead!!
+HLStats -T 1 -b lm/bigram.lm -o -t 1 dictionary/dictionary.wrd labels/words.mlf > logs/hlstats.log
+HBuild -T 1 -n lm/bigram.lm dictionary/dictionary.wrd lm/bigram.lat > logs/hbuild.log
 
 # clean up
 rm -f tmp/*
