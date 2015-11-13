13ab06bfe82df8a64aa32d02326fbe9d14290082
Mann <chanmannlim@gmail.com>
2015-11-13 10:41:40 -0600

    viterbi alignment

--- init.sh
+++ init.sh
@@ -10,7 +10,7 @@ HCompV \
  -S scripts/mfclist models/proto \
  > logs/hcompv_hmm_0.log
 
-# initialize each hmm with the global estimated mean and variance
+# initialize each hmm with the global estimated mean and variance in HMM macro file
 head -n 3 models/hmm_0/proto > models/hmm_0/models.mmf
 for phone in `cat phones/all.phe`
 do
@@ -34,13 +34,13 @@ HERest \
  -S scripts/mfclist -I labels/phoneme.mlf phones/all.phe \
  > logs/herest_hmm_0.log
 
-# update HMM parameters
+# update HMM parameters to fix silence model
 HHEd \
  -T 1 -H models/hmm_0/models.mmf -M models/hmm_0 \
  commands/sil.hed phones/all.phe \
  > logs/hhed_hmm_0.log
 
-# parameter re-estimation
+# 2x parameter re-estimation right after fixing silence model
 HERest \
  -T 1 -H models/hmm_0/models.mmf -M models/hmm_0 \
  -C configs/herest.conf -w 1 -t 120.0 60.0 960.0 \
@@ -60,16 +60,17 @@ HVite \
  dictionary/dictionary.dct.withsil phones/all.phe \
  > logs/hvite_hmm_0.log
 
-# HERest \
-#  -T 1 -H models/hmm_0/models.mmf -M models/hmm_0 \
-#  -C configs/herest.conf -w 1 -t 120.0 60.0 960.0 \
-#  -S scp/khmer.scp -I mlf/khmeralgn.mlf phe/khmer.phe \
-#  > models/hmm_0/herest.log
-# HERest \
-#  -T 1 -H models/hmm_0/models.mmf -M models/hmm_0 \
-#  -C configs/herest.conf -w 1 -t 120.0 60.0 960.0 \
-#  -S scp/khmer.scp -I mlf/khmeralgn.mlf phe/khmer.phe \
-#  > models/hmm_0/herest.log
+# 2x parameter re-estimation right after viterbi alignment
+HERest \
+ -T 1 -H models/hmm_0/models.mmf -M models/hmm_0 \
+ -C configs/herest.conf -w 1 -t 120.0 60.0 960.0 \
+ -S scripts/mfclist -I labels/phoneme_with_alignment.mlf phones/all.phe \
+ > logs/herest_hmm_0.log
+HERest \
+ -T 1 -H models/hmm_0/models.mmf -M models/hmm_0 \
+ -C configs/herest.conf -w 1 -t 120.0 60.0 960.0 \
+ -S scripts/mfclist -I labels/phoneme_with_alignment.mlf phones/all.phe \
+ > logs/herest_hmm_0.log
 
 
 # cp models/hmm_0/models.mmf models/am2/models.mmf
