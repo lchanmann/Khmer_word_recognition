c96c3f89c02e01d9e1dc205530b0e1573e70bead
Mann <chanmannlim@gmail.com>
2015-11-24 15:03:39 -0600

    update hcopy config

--- decode.sh
+++ decode.sh
@@ -1,8 +1,8 @@
 # decode.sh - decode with viterbi algorithm and generate hypothesis results
 
 HVite \
- -T 1 -z lat -l lab -i tests/$1/output_$2.mlf \
- -C configs/hvite.conf -q Atvaldmnr -s 2.4 -p -1.2 \
+ -T 1 -l '*' -i tests/$1/output_$2.mlf \
+ -z zoo -q Atvaldmnr -s 2.4 -p -1.2 \
  -S tests/$1/mfclist_$2 -H models/models.mmf -w lm/word_network.lat \
  dictionary/dictionary.dct phones/all.phe > tests/$1/$2.log
 
