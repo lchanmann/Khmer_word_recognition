2bc2dc78b48a6576e4392107159356b1c506c223
Mann <chanmannlim@gmail.com>
2016-02-10 15:26:26 -0600

    start dnn with full state viterbi alignment

---
+++ dnn.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+# -----------------------------------
+# Project   : Khmer_word_recognition
+# Author    : Chanmann Lim
+#
+# Changelogs:
+#   - 02/10/2016  : file created.
+# -----------------------------------
+
+# exit on error
+set -e
+
+# E_VARS
+E_USAGE="Usage: $0 \$directory"
+
+# global variables
+SCRIPT_NAME=$0
+DIR=
+MFCLIST=
+MODELS_MMF=
+HMMLIST=
+
+# show usage
+show_usage() {
+  echo $E_USAGE >&2
+}
+
+# setup
+setup() {
+  bash ./args_check.sh 1 $@ || (show_usage && exit 1)
+  DIR=$1
+  MFCLIST=$DIR/mfclist_trn
+  MODELS_MMF=$DIR/models/models.mmf
+  HMMLIST=$DIR/hmmlist
+  
+  mkdir -p $DIR/dnn
+  
+  # stdout
+  echo "$SCRIPT_NAME -> setup()"
+  echo "  DIR: $DIR"
+  echo
+}
+
+# state-to-frame alignment
+state2frame_align() {
+  echo "$SCRIPT_NAME -> state2frame_align()"
+  echo
+  
+  # viterbi alignment # -m -b SIL -o SW -y lab \
+  HVite -A -D -V \
+    -T 1 -a -l '*' -I labels/words.mlf -i $DIR/dnn/train.align.mmf \
+    -C configs/hvite.conf -f -o MW -b SIL -y lab \
+    -S $MFCLIST -H $MODELS_MMF \
+    dictionary/dictionary.dct.withsil $HMMLIST \
+    > $DIR/dnn/hvite_state2frame_align.log
+}
+
+# ------------------------------------
+# dnn.sh - train dnn-hmm models
+#
+#   $1 : DIR
+# ------------------------------------
+
+  setup experiments/step_by_step # $@
+  state2frame_align
+  
